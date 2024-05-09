9a44b25ac2f7fb8d79a4c5a2cc58536df3b4a818
Willem Klumpenhouwer <willemjjk@gmail.com>
2024-05-09 09:13:25 -0600

    Updated settings profile.

---
+++ dash/dash/settings/development.py
@@ -0,0 +1,5 @@
+from dash.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS.append("ted.klumpen.town")
---
+++ dash/dash/settings/production.py
@@ -0,0 +1,4 @@
+from dash.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
