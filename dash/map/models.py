66fd14be64f7caf82700db9aad8879963d03a6cc
Willem Klumpenhouwer <willemjjk@gmail.com>
2024-05-23 15:04:19 -0600

    Added some basic models.

--- dash/map/models.py
+++ dash/map/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class DataStory(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Reference(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
